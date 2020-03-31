7cd82e398d2e5a3ef7c6851cabbaeb5270f71160
YuanchenHua <657223675@qq.com>
2020-04-01 03:49:04 +1100

    to lesson 7

--- lesson04-简单回归案例实战/gd.py
+++ lesson04-简单回归案例实战/gd.py
@@ -16,6 +16,11 @@ def step_gradient(b_current, w_current, points, learningRate):
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
+        # 这里就可以看出来，很多数据的时候，是把全部数据带进去求导数，平均一下，进行梯度更新
+        # 而epoch的数量，其实就是 全部数据带进去后 梯度更新了几次
+        # batch的大小，就是把一个batch的数据，全部带进去求导数，平均一下，进行梯度更新，
+        # 下一个batch，再全部带进去求导数，平均一下，进行梯度更新
+        # 全部batch更新完了，算一个epoch
         b_gradient += -(2/N) * (y - ((w_current * x) + b_current))
         w_gradient += -(2/N) * x * (y - ((w_current * x) + b_current))
     new_b = b_current - (learningRate * b_gradient)
@@ -30,7 +35,7 @@ def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_i
     return [b, m]
 
 def run():
-    points = np.genfromtxt("data.csv", delimiter=",")
+    points = np.genfromtxt("lesson04-简单回归案例实战/data.csv", delimiter=",")
     learning_rate = 0.0001
     initial_b = 0 # initial y-intercept guess
     initial_m = 0 # initial slope guess
